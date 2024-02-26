3ca88229f4412a064fa953e8c8372740bfd5d47a
M-Levere <mattlevere1@gmail.com>
2024-02-25 23:57:18 -0500

    Made the array size 200

--- src/GenerateNewArray.js
+++ src/GenerateNewArray.js
@@ -1,7 +1,7 @@
 export default function generateNewArray() {
   let array = [];
 
-  for (let i = 0; i < 250; i++) {
+  for (let i = 0; i < 200; i++) {
     array.push(5 + Math.floor(Math.random() * 300));
   }
 
