f7390a0a3add940babf7f549b91fe1ab51dc7394
M-Levere <mattlevere1@gmail.com>
2024-02-26 15:12:07 -0500

    a

--- src/GenerateNewArray.js
+++ src/GenerateNewArray.js
@@ -1,7 +1,7 @@
 export default function generateNewArray() {
   let array = [];
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 150; i++) {
     array.push(5 + Math.floor(Math.random() * 300));
   }
 
